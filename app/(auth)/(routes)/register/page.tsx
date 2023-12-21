b08f6ab3c57ae58419998171453f73095e7c35a9
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-12-20 22:16:36 -0300

    folders setup

---
+++ app/(auth)/(routes)/register/page.tsx
@@ -0,0 +1,8 @@
+
+const RegiterPage = () => {
+    return (
+        <div>register</div>
+    )
+}
+
+export default RegiterPage
--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -5,8 +5,5 @@ export default function Home() {
   return (
     <div>
       <p className='text-3xl font-bold text-indigo-500'>Hello World</p>
-      <Button>
-        Hello
-      </Button>
     </div>)
 }

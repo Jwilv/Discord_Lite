ad3bc9efaa34a26cf2aaa5b8f7c7d174120b6b99
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-12-21 21:10:05 -0300

    ModeToggle

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,11 +1,11 @@
-import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
 
 
 export default function Home() {
   return (
     <div>
-      <p className='text-3xl font-bold text-indigo-500'>Hello World</p>
       <UserButton afterSignOutUrl="/"/>
+      <ModeToggle /> 
     </div>)
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 import { Open_Sans } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from '@/components/providers/theme-provider'
+import { cn } from '@/lib/utils'
 
 const inter = Open_Sans({ subsets: ['latin'] })
 
@@ -19,11 +20,13 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <body className={inter.className}>
+        <body className={cn(
+          inter.className,
+          'bg-white dark:bg-[#313338]'
+        )}>
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
-            forcedTheme='dark'
             enableSystem={false}
             storageKey='discord-theme'
 

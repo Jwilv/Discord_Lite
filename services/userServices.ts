439568e2fc14e2b5ea2cca7a1576de6cfab506fc
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-12-24 17:22:38 -0300

    user services

---
+++ services/userServices.ts
@@ -0,0 +1,24 @@
+import { db } from '@/lib/db'
+import { User } from '@clerk/nextjs/server';
+
+export const getProfileById = async (id: string) => {
+    return await db.profile.findUnique({
+        where: {
+            userId: id
+        }
+    });
+};
+
+export const createNewProfile = async (user: User) => {
+
+    const newProfile = await db.profile.create({
+        data: {
+            userId: user.id,
+            name: `${user.firstName} ${user.lastName}`,
+            imageUrl: user.imageUrl,
+            email: user.emailAddresses[0].emailAddress
+        }
+    });
+
+    return newProfile
+};

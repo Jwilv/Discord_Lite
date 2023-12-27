40117582912e9572e562bce2aa2f140e9c58a360
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-12-27 19:43:50 -0300

    form

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -2,6 +2,7 @@ import { redirect } from "next/navigation";
 
 import { initialProfile } from "@/lib/initial-profile"
 import { getServerByProfileId } from "@/services/serverServices";
+import { InitialModal } from "@/components/modals/initial-modal";
 
 
 const SetupPage = async () => {
@@ -11,9 +12,7 @@ const SetupPage = async () => {
 
     if (server) return redirect(`/server/${server.id}`)
 
-    return (
-        <div>Create server</div>
-    )
+    return <InitialModal /> 
 }
 
 export default SetupPage

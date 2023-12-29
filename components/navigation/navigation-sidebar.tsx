0b98125f8ca5695e267b881fb250e486fe86f6ac
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-12-29 20:04:40 -0300

    Tooltip

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,4 +1,5 @@
 import { currentUser } from "@/lib/current-user";
+import { getServersByUserId } from "@/services/server";
 import { redirect } from 'next/navigation'
 
 
@@ -8,8 +9,12 @@ export const NavigationSidebar = async () => {
 
     if (!profile) return redirect('/');
 
-    
+    const servers = await getServersByUserId(profile.userId!);
+
     return (
-        <div>NavigationSidebar</div>
+        <div
+            className="space-y-4 flex flex-col items-center h-full text-primary
+                w-full dark:bg-[#1E1F22] py-3"
+        >NavigationSideba</div>
     )
 }

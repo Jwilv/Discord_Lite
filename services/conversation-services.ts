199bc47c8690d88ef4e06cb518b78fd8558015c9
Jwilv <juan.ignacio.wilvers@gmail.com>
2024-01-10 18:45:59 -0300

    createConversation and try catch

--- services/conversation-services.ts
+++ services/conversation-services.ts
@@ -2,18 +2,41 @@ import { db } from "@/lib/db"
 
 
 export const findConversation = async (memberOneId: string, memberTwoId: string) => {
-    const conversation = await db.conversation.findFirst({
-        where: {
-            AND: [
-                { memberOneId },
-                { memberTwoId }
-            ]
-        },
-        include: {
-            memberOne: true,
-            memberTwo: true,
-        }
-    });
+    try {
+        const conversation = await db.conversation.findFirst({
+            where: {
+                AND: [
+                    { memberOneId },
+                    { memberTwoId }
+                ]
+            },
+            include: {
+                memberOne: true,
+                memberTwo: true,
+            }
+        });
 
-    return conversation
+        return conversation
+    } catch (error) {
+        console.log("[FIND_CONVERSATION]");
+        return null
+    }
+}
+
+export const createConversation = async (memberOneId: string, memberTwoId: string) => {
+    try {
+        return await db.conversation.create({
+            data: {
+                memberOneId,
+                memberTwoId
+            },
+            include: {
+                memberOne: true,
+                memberTwo: true
+            }
+        });
+    } catch (error) {
+        console.log("[CREATE_CONVERSATION]");
+        return null
+    }
 }

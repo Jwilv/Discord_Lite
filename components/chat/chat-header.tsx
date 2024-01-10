f47290f6a3a7cf7f263e5d8719c6f0c031eb14a4
Jwilv <juan.ignacio.wilvers@gmail.com>
2024-01-10 17:25:16 -0300

    MovileToggle

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,4 +1,5 @@
 import { Hash, Menu } from "lucide-react";
+import { MovileToggle } from "../movile-toggle";
 
 interface Props {
     imageUrl?: string;
@@ -18,7 +19,7 @@ function ChatHeader({
             className="text-md font-semibold px-3 flex items-center h-12 
             border-neutral-200 dark:border-neutral-800 border-b-2"
         >
-            <Menu />
+            <MovileToggle serverId={serverId} />
             {
                 type === 'channel' && (
                     <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />

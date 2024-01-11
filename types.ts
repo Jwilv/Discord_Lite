c36b84d6eff9b47b19db928ef149436f116c1e56
Jwilv <juan.ignacio.wilvers@gmail.com>
2024-01-10 21:10:43 -0300

    ioHandler

--- types.ts
+++ types.ts
@@ -1,5 +1,16 @@
+import { Server as NetServer, Socket } from 'net'
+import { NextApiResponse } from 'next'
+import { Server as SocketIOServer } from 'socket.io'
 import { Channel, Member, Profile, Server } from "@prisma/client"
 
 export type ServerWithMembersWhithProfiles = Server & {
     members: Member[]
-}
+};
+
+export type NextApiResponseServerIO = NextApiResponse & {
+    socket: Socket & {
+        server : NetServer & {
+            io : SocketIOServer
+        };
+    };
+};

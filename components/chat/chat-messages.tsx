a9847f5f295eafb8b67a8bd39ebb02b4d5bd1035
Jwilv <juan.ignacio.wilvers@gmail.com>
2024-01-12 14:53:20 -0300

    chat message update

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,5 +1,10 @@
+"use client";
+
 import { Member } from "@prisma/client";
 import ChatWelcome from "./chat-welcome";
+import { useChatQuery } from "@/hooks/use-chat-query";
+import { RedirectToUserProfile } from "@clerk/nextjs";
+import { Loader2, ServerCrash } from "lucide-react";
 
 
 interface props {
@@ -24,6 +29,45 @@ const ChatMessages = ({
     paramValue,
     type,
 }: props) => {
+
+    const queryKey = `chat:${chatId}`
+
+    const {
+        data,
+        fetchNextPage,
+        hasNextPage,
+        isFetchingNextPage,
+        status
+    } = useChatQuery({
+        apiUrl,
+        paramKey,
+        paramValue,
+        queryKey
+    });
+
+
+    if (status === 'loading') {
+        return (
+            <div className="flex flex-col flex-1 justify-center items-center">
+                <Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4" />
+                <p className="text-xs text-zinc-500 dark:text-zinc-400">
+                    Loading messages...
+                </p>
+            </div>
+        )
+    }
+
+    if (status === 'error') {
+        return (
+            <div className="flex flex-col flex-1 justify-center items-center">
+                <ServerCrash className="h-7 w-7 text-zinc-500 my-4" />
+                <p className="text-xs text-zinc-500 dark:text-zinc-400">
+                    Something went wrong
+                </p>
+            </div>
+        )
+    }
+
     return (
         <div className="flex-1 flex flex-col py-4 overflow-y-auto">
             <div className="flex-1" />

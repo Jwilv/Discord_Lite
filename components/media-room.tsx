da7ce1431a54e1abfc821de6e21f974020f78a0f
Jwilv <juan.ignacio.wilvers@gmail.com>
2024-01-13 15:01:28 -0300

    live

--- components/media-room.tsx
+++ components/media-room.tsx
@@ -1,56 +1,58 @@
-"use client"
+"use client";
 
-import { useEffect, useState } from 'react'
-import { LiveKitRoom, VideoConference } from '@livekit/components-react'
-import '@livekit/components-styles'
-import { Channel } from 'diagnostics_channel'
-import { Loader2 } from 'lucide-react'
-import { currentUser } from '../lib/current-user';
+import { useEffect, useState } from "react";
+import { LiveKitRoom, VideoConference } from "@livekit/components-react";
+import "@livekit/components-styles";
+import { Channel } from "@prisma/client";
+import { useUser } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
 
 interface MediaRoomProps {
     chatId: string;
     video: boolean;
     audio: boolean;
-}
+};
 
-export const MediaRoom = async ({
+export const MediaRoom = ({
     chatId,
     video,
     audio
 }: MediaRoomProps) => {
-
-    const user = await currentUser();
-    const [token, setToken] = useState('');
+    const { user } = useUser();
+    const [token, setToken] = useState("");
 
     useEffect(() => {
+        if (!user?.firstName || !user?.lastName) return;
 
-        if (!user) return
+        const name = `${user.firstName} ${user.lastName}`;
 
         (async () => {
             try {
-                const resp = await fetch(`/api/livekit?room=${chatId}&username=${user.name}`);
+                const resp = await fetch(`/api/livekit?room=${chatId}&username=${name}`);
                 const data = await resp.json();
                 setToken(data.token);
-            } catch (error) {
-                console.log(error)
+            } catch (e) {
+                console.log(e);
             }
-        })
+        })()
+    }, [user?.firstName, user?.lastName, chatId]);
 
-    }, [user?.name, chatId])
-
-    if (token === '') {
-        <div className='flex flex-col flex-1 justify-center items-center'>
-            <Loader2 className='w-7 h-7 animate-spin text-zinc-500' />
-            <p className='text-xs text-zinc-500 dark:text-zinc-400 '>
-                Loading...
-            </p>
-        </div>
+    if (token === "") {
+        return (
+            <div className="flex flex-col flex-1 justify-center items-center">
+                <Loader2
+                    className="h-7 w-7 text-zinc-500 animate-spin my-4"
+                />
+                <p className="text-xs text-zinc-500 dark:text-zinc-400">
+                    Loading...
+                </p>
+            </div>
+        )
     }
-
 
     return (
         <LiveKitRoom
-            data-lk-theme='default'
+            data-lk-theme="default"
             serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_URL}
             token={token}
             connect={true}
@@ -60,5 +62,4 @@ export const MediaRoom = async ({
             <VideoConference />
         </LiveKitRoom>
     )
-
 }
